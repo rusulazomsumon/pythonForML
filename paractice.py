4aedab088d2f2155e84f0b2e24298b8bde05d860
sumon <18103020@iubat.edu>
2023-02-01 13:17:04 +0600

    paractice code added

---
+++ paractice.py
@@ -0,0 +1,48 @@
+# Variables and data types
+
+# declare variables and assign values
+name = "Sumon"
+age = 30
+is_student = False
+
+# print variable values
+print("Name:", name)
+print("Age:", age)
+print("Is student:", is_student)
+
+# Arithmetic Operators
+
+# perform arithmetic operations
+addition = 2 + 3
+subtraction = 5 - 2
+multiplication = 2 * 3
+division = 6 / 2
+modulo = 7 % 2
+
+# print results
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Modulo:", modulo)
+
+# Control Structures
+
+# if statement example
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+# for loop example
+for i in range(1, 6):
+    print("Loop iteration:", i)
+
+# Functions
+
+# define a function
+def greet(name):
+    print("Hello, " + name + "!")
+
+# call the function
+greet("Sumon")
